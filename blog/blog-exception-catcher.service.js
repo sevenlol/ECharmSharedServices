58831dd8625561ed6d2633e1c9e130706b1255eb
ian_lin <ian_lin@gemteks.com>
2015-04-27 13:55:40 +0800

    [master] Implement the exception catcher for Blog System

--- blog/blog-exception-catcher.service.js
+++ blog/blog-exception-catcher.service.js
@@ -27,7 +27,27 @@ function blogExceptionCatcherService() {
      *                   error message
      */
     function catcher(error, method) {
+        if (!validateError(error) || error.status === 0)
+            return new BlogError('Some Unknown Error Occurred!', error);
 
+        var errorMessage;
+
+        if (error.status === 400) {
+            errorMessage = 'Something is wrong with the request!';
+        } else if (error.status === 401) {
+            errorMessage = 'Please sign in first!';
+        } else if (error.status === 403) {
+            errorMessage = 'You are not authorized for this action!';
+        } else if (error.status === 409) {
+            // TODO add more conditions for creating conflict Rating
+            errorMessage = 'You already rated this article!';
+        } else if (error.status === 500) {
+            errorMessage = 'Something is wrong with the server!';
+        } else {
+            errorMessage = 'Some Unknown Error Occurred!';
+        }
+
+        return new BlogError(errorMessage, error);
     }
 
     /*
@@ -35,10 +55,34 @@ function blogExceptionCatcherService() {
      * @param {String} message the message describing the error
      * @returns
      */
-    function BlogError(message) {
+    function BlogError(message, error) {
         Error.captureStackTrace(this);
         this.message = message;
         this.name = "BlogError";
+
+        if (angular.isObject(error.data) && error.data !== null)
+            this.errorBody = error.data;
     }
     BlogError.prototype = Object.create(Error.prototype);
+
+
+    /*
+     * Private functions
+     */
+
+    /*
+     * @desc Validate if the error object is legit
+     * @param {Object} error the error object from the HTTP request
+     * @returns {Boolean} return true if the error object is legit
+     *                    return false otherwise
+     */
+    function validateError(error) {
+        if (!angular.isObject(error))
+            return false;
+
+        if (!angular.isNumber(error.status))
+            return false;
+
+        return true;
+    }
 }
