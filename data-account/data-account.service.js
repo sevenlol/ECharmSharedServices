1cfd1994d67e1ea53c5ead42fc9ebca128f5d516
sevenlol <sevenlol1007@gmail.com>
2015-05-09 21:37:54 +0800

    [master] Modify account service

--- data-account/data-account.service.js
+++ data-account/data-account.service.js
@@ -32,31 +32,13 @@ function accountService($http, accountResponseHandlerCatcherService, accountVali
     };
 
     var VALIDATOR = {
-        ARRAY : {
-            USER : accountValidatorService.responseValidator
-                        .userAccountValidator
+        ARRAY : accountValidatorService.responseValidator
+                        .arbitraryAccountValidator
                         .validateArray,
-            DOCTOR : accountValidatorService.responseValidator
-                        .doctorAccountValidator
-                        .validateArray,
-            ADMIN : accountValidatorService.responseValidator
-                        .adminAccountValidator
-                        .validateArray
-        },
-        OBJECT : {
-            USER : accountValidatorService
-                         .responseValidator
-                         .userAccountValidator
-                         .validateObject,
-            DOCTOR : accountValidatorService
-                         .responseValidator
-                         .doctorAccountValidator
-                         .validateObject,
-            ADMIN : accountValidatorService
-                         .responseValidator
-                         .adminAccountValidator
-                         .validateObject
-        }
+        OBJECT : accountValidatorService
+                        .responseValidator
+                        .arbitraryAccountValidator
+                        .validateObject
     };
     var RES_FAILED_CALLBACK = accountResponseHandlerCatcherService.requestFailed;
     var REQ_COMPLETED_CALLBACK = {
@@ -130,10 +112,7 @@ function accountService($http, accountResponseHandlerCatcherService, accountVali
         if (!url)
             throw new Error(accountExceptionCatcherService.DEFAULT_ERROR_MESSAGE);
 
-        // TODO refactor this
-        return $http.get(url)
-                .then(getAllAccountSucceeded)
-                .catch(RES_FAILED_CALLBACK);
+        return getHttpPromise(HTTP_METHOD.GET, url, null, VALIDATOR.ARRAY, VALIDATOR.OBJECT);
     }
 
     /*
@@ -178,100 +157,20 @@ function accountService($http, accountResponseHandlerCatcherService, accountVali
      * @param {Object} account the account object
      * @returns {Object} a $http promise with callbacks set
      */
-    function getHttpPromise(method, url, account, VALIDATE_ARRAY, VALIDATE_OBJECT) {
+    function getHttpPromise(method, url, account, validateArray, validateObject) {
         if (method === HTTP_METHOD.GET) {
             return $http.get(url)
-                        .then(
-                            (function (validateArray, validateObject) {
-                                return function(response) {
-                                    var arrayCallback;
-                                    var objectCallback;
-
-                                    if (!angular.isObject(response) || response === null)
-                                        return null;
-
-                                    if (!angular.isObject(response.data) || response.data === null)
-                                        return null;
-
-                                    if (response.data.user_type === USER_TYPE.USER) {
-                                        arrayCallback = validateArray.USER;
-                                        objectCallback = validateObject.USER;
-                                    } else if (response.data.user_type === USER_TYPE.DOCTOR) {
-                                        arrayCallback = validateArray.DOCTOR;
-                                        objectCallback = validateObject.DOCTOR;
-                                    } else if (response.data.user_type === USER_TYPE.ADMIN) {
-                                        arrayCallback = validateArray.ADMIN;
-                                        objectCallback = validateObject.ADMIN;
-                                    } else {
-                                        return null;
-                                    }
-
-                                    return REQ_COMPLETED_CALLBACK.GET(response, arrayCallback, objectCallback);
-                                };
-                            })(VALIDATE_ARRAY, VALIDATE_OBJECT)
-                        )
-                        .catch(RES_FAILED_CALLBACK);
+                    .then(
+                        (function (validateArray, validateObject) {
+                            return function(response) {
+                                return REQ_COMPLETED_CALLBACK.GET(response, validateArray, validateObject);
+                            };
+                        })(validateArray, validateObject)
+                    )
+                    .catch(RES_FAILED_CALLBACK);
         }
 
         // wrong HTTP method
         throw new Error(accountExceptionCatcherService.DEFAULT_ERROR_MESSAGE);
-    }
-
-    /* TODO REFACTOR AND MOVE THE FOLLOWING FUNCTIONS */
-    function getAllAccountSucceeded(response) {
-        if (!angular.isObject(response) || response === null)
-            return null;
-
-        if (!angular.isNumber(response.status))
-            return null;
-
-        if (response.status === 204)
-            return null;
-
-        if (response.status === 200) {
-            if (angular.isArray(response.data)) {
-                // array
-                return validateArbitraryAccountArray(response.data);
-            } else if (angular.isObject(response.data)) {
-                // object
-                return validateArbitraryAccount(response.data);
-            }
-        }
-
-        return null;
-    }
-
-    function validateArbitraryAccountArray(accountArray) {
-        // check the comment array
-        if (!angular.isArray(accountArray) || accountArray.length <= 0)
-            return null;
-
-        for (var i = 0; i < accountArray.length; i++) {
-            var comment = validateArbitraryAccount(accountArray[i]);
-            if (comment === null) {
-                // remove invalid array
-                accountArray.splice(i--, 1);
-            }
-        }
-
-        if (accountArray.length === 0)
-            return null;
-
-        return accountArray;
-    }
-
-    function validateArbitraryAccount(account) {
-        if (!account.user_type)
-            return null;
-
-        if (account.user_type === 'USER') {
-            return VALIDATOR.OBJECT.USER(account);
-        } else if (account.user_type === 'DOCTOR') {
-            return VALIDATOR.OBJECT.DOCTOR(account);
-        } else if (account.user_type === 'ADMIN') {
-            return VALIDATOR.OBJECT.ADMIN(account);
-        }
-
-        return null;
     }
 }
