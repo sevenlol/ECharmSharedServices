bcfd50a87e17c25a7e6484fd4695105f2244d6f2
ian_lin <ian_lin@gemteks.com>
2015-06-11 11:49:22 +0800

    [master] Add question service function signature

--- data-askdoctor/data-askdoctor-question.service.js
+++ data-askdoctor/data-askdoctor-question.service.js
@@ -5,23 +5,108 @@
 (function() {
     "use strict";
 
-	//module getter
-	angular
-	    .module('data.askdoctor')
-	    .factory('askdoctorQuestionService', askdoctorQuestionService);
+    //module getter
+    angular
+        .module('data.askdoctor')
+        .factory('askdoctorQuestionService', askdoctorQuestionService);
 
-	askdoctorQuestionService.$inject = [
-	];
+    askdoctorQuestionService.$inject = [
+        '$http',
+        'askdoctorResponseHandlerService',
+        'askdoctorValidatorService',
+        'askdoctorExceptionCatcherService',
+        'valueService',
+        'Logger'
+    ];
 
-	function askdoctorQuestionService() {
-	    var service = {
+    function askdoctorQuestionService($http, askdoctorResponseHandlerService, askdoctorValidatorService,
+                                    askdoctorExceptionCatcherService, valueService, Logger) {
+        var SERVER_URL = valueService.SERVER_URL.ASKDOCTOR;
+        var HTTP_METHOD = valueService.HTTP_METHOD;
+        var REQ_VALIDATOR = {
+            FILLED : askdoctorValidatorService.validator.REQ.FILLED.QUESTION,
+            NOT_EMPTY : askdoctorValidatorService.validator.REQ.NOT_EMPTY.QUESTION
+        };
+        var RES_VALIDATOR = {
+            ARRAY : askdoctorValidatorService.validator.RES.ARRAY.QUESTION,
+            OBJECT : askdoctorValidatorService.validator.RES.OBJECT.QUESTION
+        };
+        var RES_FAILED_CALLBACK = askdoctorResponseHandlerService.requestFailed;
+        var REQ_COMPLETED_CALLBACK = {
+            GET : askdoctorResponseHandlerService.getRequestCompleted,
+            POST : askdoctorResponseHandlerService.postRequestCompleted,
+            PUT : askdoctorResponseHandlerService.putRequestCompleted,
+            PATCH : askdoctorResponseHandlerService.patchRequestCompleted,
+            DELETE : askdoctorResponseHandlerService.deleteRequestCompleted
+        };
+        // Logger object
+        var logger = Logger.getInstance('app - data - askdoctor - question');
+        var service = {
+            /* Create */
+            createQuestion          :  createQuestion,
 
-	    };
-	    return service;
+            /* Read */
+            readAllQuestion         :  readAllQuestion,
+            readQuestionInCategory   :  readQuestionInCategory,
+            readQuestion            :  readQuestion,
 
-	    /*
-	     * functions
-	     */
-	}
+            /* Read By Field */
+            readQuestionByQuestionerId : readQuestionByQuestionerId,
+
+            /* Update */
+            updateQuestion          :  updateQuestion,
+            partiallyUpdateQuestion :  partiallyUpdateQuestion,
+
+            /* Delete */
+            deleteAllQuestion       :  deleteAllQuestion,
+            deleteQuestionInCategory :  deleteQuestionInCategory,
+            deleteQuestion          :  deleteQuestion
+        };
+        return service;
+
+        /*
+         * functions
+         */
+
+        function createQuestion() {
+            // body...
+        }
+
+        function readAllQuestion() {
+            // body...
+        }
+
+        function readQuestionInCategory() {
+            // body...
+        }
+
+        function readQuestion() {
+            // body...
+        }
+
+        function readQuestionByQuestionerId() {
+            // body...
+        }
+
+        function updateQuestion() {
+            // body...
+        }
+
+        function partiallyUpdateQuestion() {
+            // body...
+        }
+
+        function deleteAllQuestion() {
+            // body...
+        }
+
+        function deleteQuestionInCategory() {
+            // body...
+        }
+
+        function deleteQuestion() {
+            // body...
+        }
+    }
 
 })();
