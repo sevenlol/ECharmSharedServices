22524b1710ece8a4328c037e092dd87a06a9ca23
sevenlol <sevenlol1007@gmail.com>
2015-09-03 23:28:28 +0800

    [master] Update category list

--- value/category.value.js
+++ value/category.value.js
@@ -9,7 +9,8 @@
             { name : 'plasticsurgeon', text : '整形外科' },
             { name : 'cosmeticsurgeon', text : '醫學美容' },
             { name : 'dermatologist', text : '皮膚科' },
-            { name : 'obstetrician', text : '婦科' },
+            { name : 'pediatrician', text : '小兒科' },
+            { name : 'OBSGYN', text : '婦產科' },
             { name : 'others', text : '其他醫師' }
         ])
         // for askdoctor system
@@ -19,6 +20,7 @@
             { name : 'dermatology', text : '皮膚科' },
             { name : 'gynecology', text : '婦科' },
             { name : 'obstetrics', text : '產科' },
+            { name : 'pediatrics', text : '小兒科' },
             { name : 'others', text : '其他醫學問題' }
         ])
         // for blog system
@@ -28,6 +30,7 @@
             { name : 'dermatology', text : '皮膚科' },
             { name : 'gynecology', text : '婦科' },
             { name : 'obstetrics', text : '產科' },
+            { name : 'pediatrics', text : '小兒科' },
             { name : 'others', text : '其他醫學文章' }
         ]);
 
