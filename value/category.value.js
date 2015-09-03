0df2817ebd9d7e97b569edd3ddede87e5a8b2088
sevenlol <sevenlol1007@gmail.com>
2015-09-03 21:50:18 +0800

    [master] Update category value

--- value/category.value.js
+++ value/category.value.js
@@ -5,18 +5,30 @@
         .module('app.value')
         // for doctor account in account system
         .value('accountCategoryNameTextList', [
-            { name : 'Category_1', text : '類別1' },
-            { name : 'Category_2', text : '類別2' }
+            { name : 'PGY', text : '不分科' },
+            { name : 'plasticsurgeon', text : '整形外科' },
+            { name : 'cosmeticsurgeon', text : '醫學美容' },
+            { name : 'dermatologist', text : '皮膚科' },
+            { name : 'obstetrician', text : '婦科' },
+            { name : 'others', text : '其他醫師' }
         ])
         // for askdoctor system
         .value('askDoctorCategoryNameTextList', [
-            { name : 'Category_1', text : '類別1' },
-            { name : 'Category_2', text : '類別2' }
+            { name : 'plasticsurgery', text : '整形外科' },
+            { name : 'cosmeticsurgery', text : '醫學美容' },
+            { name : 'dermatology', text : '皮膚科' },
+            { name : 'gynecology', text : '婦科' },
+            { name : 'obstetrics', text : '產科' },
+            { name : 'others', text : '其他醫學問題' }
         ])
         // for blog system
         .value('blogCategoryNameTextList', [
-            { name : 'Category_1', text : '類別1' },
-            { name : 'Category_2', text : '類別2' }
+            { name : 'plasticsurgery', text : '整形外科' },
+            { name : 'cosmeticsurgery', text : '醫學美容' },
+            { name : 'dermatology', text : '皮膚科' },
+            { name : 'gynecology', text : '婦科' },
+            { name : 'obstetrics', text : '產科' },
+            { name : 'others', text : '其他醫學文章' }
         ]);
 
 })();
